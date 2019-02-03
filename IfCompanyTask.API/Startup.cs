a4788e14a7220e5f9d5d6cec559c3f283ab85420
Ahmet Mesut Sarıyer <amesutsariyer@outlook.com>
2019-02-03 22:04:47 +0300

    Swagger implementing API project

--- IfCompanyTask.API/Startup.cs
+++ IfCompanyTask.API/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace IfCompanyTask.API
 {
@@ -24,12 +25,47 @@ namespace IfCompanyTask.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info
+                {
+                    Version = "v1",
+                    Title = "IF Company Task Services version 1.0",
+                    Description = "If Company Task Project API",
+                    TermsOfService = "None",
+                    Contact = new Contact
+                    {
+                        Name = "Ahmet Mesut Sarıyer",
+                        Email ="ahmetmesutsariyer@gmail.com",
+                        Url = "https://github.com/amesutsariyer"
+                    },
+                    License = new License
+                    {
+                        Name = "Use under LICX",
+                        Url = "https://github.com/amesutsariyer"
+                    }
+                });
+            });
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+
+            app.UseDefaultFiles();
+            //Serve files inside of web root
+            app.UseStaticFiles();
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "If Company");
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
